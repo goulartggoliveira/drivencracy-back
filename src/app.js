bed07ff8b190edce554d2c91e0dbd65860d03d78
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-01-29 12:37:09 -0300

    deploy config

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ import cors from "cors";
 import express from "express";
 import pollRouter from "./routes/pollRouter.js";
 import choiceRouter from "./routes/choiceRouter.js";
+import dotenv from "dotenv"
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+dotenv.config()
+
 app.use(pollRouter)
 app.use(choiceRouter);
 
-const PORT = 5000
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
