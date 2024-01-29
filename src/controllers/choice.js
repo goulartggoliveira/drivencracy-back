b60774c0b744547fe0f30540b42c350382cfa077
goulartggoliveira <gustavoggoliveira98@gmail.com>
2024-01-29 11:22:17 -0300

    /post choice/:id/vote with bugs

--- src/controllers/choice.js
+++ src/controllers/choice.js
@@ -28,4 +28,37 @@ export async function choicePoll(req, res) {
     }
 }
 
+export async function choiceIdVote(req, res) {
 
+    const choiceId = req.params.id;
+
+    try {
+
+        const existingChoice = await db.collection("choices").findOne({ _id: new ObjectId(choiceId) });
+        if (!existingChoice) {
+            return res.status(404).send("Opção de voto não encontrada.");
+        }
+    
+
+        const survey = await db.collection("polls").findOne({ _id: new ObjectId(existingChoice.pollId) });
+        if (!survey) {
+            return res.status(404).send("Enquete não encontrada.");
+        }
+
+        if (dayjs(survey.expireAt) < dayjs()) {
+            return res.status(403).send("A enquete já expirou. Não é possível registrar o voto.");
+        }
+        
+        // Registrar o voto associando a data e hora da criação no back-end
+        const voteResult = await db.collection("votes").insertOne({
+            choiceId: ObjectId(choiceId),
+            createdAt: dayjs().format("YYYY-MM-DD HH:mm"),
+        });
+
+        // Retornar uma resposta adequada
+        res.status(201).send("Voto registrado com sucesso.");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Erro ao registrar voto.");
+    }
+}
